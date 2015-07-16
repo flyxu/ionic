858f1e877de8f09940699f4d2b04f04e96886b8d
rdmclin2 <rdmclin2@163.com>
2015-07-16 14:07:34 +0800

    design new info UI

--- dev/client/www/js/app.js
+++ dev/client/www/js/app.js
@@ -4,212 +4,162 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-angular.module('fcws', ['ionic', 'fcws.controllers','fcws.services'])
+angular.module('fcws', ['ionic', 'fcws.controllers', 'fcws.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider,$urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider) {
 
-$stateProvider
+  $stateProvider
     .state('auth', {
-        url: "/auth",
-        abstract: true,
-        templateUrl: "templates/auth.html",
-        onEnter: function(API,$state){
-                  // if the user is already logged in, take him to his bucketlist
-                  if (API.isSessionActive()) {
-                      $state.go('sidemenu.qbxx');
-                  }
-                }
-    })
-
-    // login page
-    .state('auth.signin', {
-        url: "/signin",
-        views: {
-            'auth-signin': {
-                templateUrl: 'templates/auth-signin.html',
-                controller: 'SignInCtrl'
-
-            }
-        }
-    })
-
-    // signup page
-    .state('auth.signup', {
-      url: '/signup',
-      views: {
-          'auth-signup': {
-              templateUrl: 'templates/auth-signup.html',
-              controller: 'SignUpCtrl'
-          }
-      }
-    })
-
-    .state('sidemenu',{
-      url: '/sidemenu',
+      url: "/auth",
       abstract: true,
-      templateUrl: 'templates/sidemenu.html',
-      onEnter: function(API,$state){
-                  // if the user is already logged in, take him to his bucketlist
-                  if (!API.isSessionActive()) {
-                      $state.go('auth.signin');
-                  }
-                }
+      templateUrl: "templates/auth.html",
+      // onEnter: function(API, $state) {
+      //   // if the user is already logged in, take him to his bucketlist
+      //   if (API.isSessionActive()) {
+      //     $state.go('sidemenu.dashboard');
+      //   }
+      // }
+    })
+
+  // login page
+  .state('auth.signin', {
+    url: "/signin",
+    views: {
+      'auth-signin': {
+        templateUrl: 'templates/auth-signin.html',
+        controller: 'SignInCtrl'
+      }
+    }
+  })
+
+  // signup page
+  .state('auth.signup', {
+    url: '/signup',
+    views: {
+      'auth-signup': {
+        templateUrl: 'templates/auth-signup.html',
+        controller: 'SignUpCtrl'
+      }
+    }
+  })
+
+  .state('sidemenu', {
+    url: '/sidemenu',
+    abstract: true,
+    templateUrl: 'templates/sidemenu.html',
+    // onEnter: function(API, $state) {
+    //     // if the user is already logged in, take him to his bucketlist
+    //     if (!API.isSessionActive()) {
+    //       $state.go('auth.signin');
+    //     }
+    //   }
       //controller: 'HomeCtrl',
-    })
+  })
 
-    .state('sidemenu.info',{
-      url: '/info',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-info.html',
-              controller: 'InfoCtrl',
-            }
-        }
-    })
-
-    .state('sidemenu.dashboard',{
-      url: '/dashboard',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-dash.html',
-              controller: 'DashboardCtrl',
-            }
-        }
-    })
-
-    .state('sidemenu.zhkz',{
-      url: '/zhkz',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-zhkz.html',
-              controller: 'ZhkzCtrl',
-              // onEnter: function($rootScope,$state){
-              //     // if the user is already logged in, take him to his bucketlist
-              //     if (!$rootScope.isSessionActive()) {
-              //         $state.go('auth.signin');
-              //     }
-              //   }
-            }
-          }
-    })
-
-    .state('sidemenu.jsxl',{
-      url: '/jsxl',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-jsxl.html',
-              controller: 'JsxlCtrl',
-              // onEnter: function($rootScope,$state){
-              //     // if the user is already logged in, take him to his bucketlist
-              //     if (!$rootScope.isSessionActive()) {
-              //         $state.go('auth.signin');
-              //     }
-              //   }
-            }
-          }
-    })
-
-     .state('sidemenu.jygl',{
-      url: '/jygl',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-jygl.html',
-              controller: 'JyglCtrl',
-              // onEnter: function($rootScope,$state){
-              //     // if the user is already logged in, take him to his bucketlist
-              //     if (!$rootScope.isSessionActive()) {
-              //         $state.go('auth.signin');
-              //     }
-              //   }
-            }
-          }
-    })
-
-    .state('sidemenu.gfdy',{
-      url: '/gfdy',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-gfdy.html',
-              controller: 'GfdyCtrl',
-              // onEnter: function($rootScope,$state){
-              //     // if the user is already logged in, take him to his bucketlist
-              //     if (!$rootScope.isSessionActive()) {
-              //         $state.go('auth.signin');
-              //     }
-              //   }
-            }
-          }
-    })
-
-.state('sidemenu.tabs', {
-  url: "/tabs",
-  views: {
-    'menuContent': {
-      templateUrl: "templates/tabs.html",
+  .state('sidemenu.dashboard', {
+    url: '/dashboard',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-dash.html',
+        controller: 'DashboardCtrl',
+      }
     }
-  }
-})
+  })
 
-.state('sidemenu.tabs.one', {
-  url: "/one",
-  views: {
-    'tab-one': {
-      templateUrl: "templates/tab1.html",
-       controller: 'TabCtrl',
+  .state('sidemenu.info', {
+    url: '/info',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-info.html',
+        controller: 'InfoCtrl'
+      }
     }
-  }
+  })
 
-})
-
-
-.state('posts', {
-      url: "/posts",
-      templateUrl: "templates/posts.html"
+  .state('post', {
+    url: "/post",
+    templateUrl: "templates/info-post.html"
   })
 
   .state('commentview', {
-        url: "/commentview",
-        templateUrl: "templates/commentview.html"
-    })
+    url: "/commentview",
+    templateUrl: "templates/commentview.html"
+  })
+
+  .state('sidemenu.zhkz', {
+    url: '/zhkz',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-zhkz.html',
+        controller: 'ZhkzCtrl'
+      }
+    }
+  })
+
+  .state('sidemenu.jsxl', {
+    url: '/jsxl',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-jsxl.html',
+        controller: 'JsxlCtrl'
+      }
+    }
+  })
+
+  .state('sidemenu.jygl', {
+    url: '/jygl',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-jygl.html',
+        controller: 'JyglCtrl'
+      }
+    }
+  })
+
+  .state('sidemenu.gfdy', {
+    url: '/gfdy',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-gfdy.html',
+        controller: 'GfdyCtrl',
+      }
+    }
+  })
+
+  .state('sidemenu.txl', {
+    url: '/txl',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home-txl.html',
+        controller: 'TxlCtrl',
+      }
+    }
+  });
 
 
-    .state('sidemenu.txl',{
-      url: '/txl',
-      views: {
-          'menuContent':{
-              templateUrl: 'templates/home-txl.html',
-              controller: 'TxlCtrl',
-              // onEnter: function($rootScope,$state){
-              //     // if the user is already logged in, take him to his bucketlist
-              //     if (!$rootScope.isSessionActive()) {
-              //         $state.go('auth.signin');
-              //     }
-              //   }
-            }
-          }
-    });
+
 
   $urlRouterProvider.otherwise('/auth/signin');
 
 })
 
 .constant('SERVER', {
-    // if using local server
+  // if using local server
   url: 'http://localhost:9804'
 
   // if using our public heroku server
-//  url: 'http://nemoworks.info:9804'
+  //  url: 'http://nemoworks.info:9804'
 });
--- dev/client/www/js/controllers.js
+++ dev/client/www/js/controllers.js
@@ -1,104 +1,102 @@
-angular.module('fcws.controllers', ['ionic','fcws.services'])
+angular.module('fcws.controllers', ['ionic', 'fcws.services'])
 
 /*
 Controller for the Splash page
 */
 .controller('SignInCtrl', function($rootScope, $scope, API, $window) {
 
-    $scope.user = {
-        email: "",
-        password: ""
-    };
+  // $scope.user = {
+  //   email: "",
+  //   password: ""
+  // };
 
-    $scope.validateUser = function () {
-        var email = this.user.email;
-        var password = this.user.password;
-        if(!email || !password) {
-          $rootScope.notify("Please enter valid credentials");
-          return false;
-        }
-        $rootScope.show('Please wait.. Authenticating');
-        API.signin({
-            email: email,
-            password: password
-        }).success(function (data) {
-            $rootScope.setToken(email); // create a session kind of thing on the client side
-            $rootScope.hide();
-            $window.location.href = ('#/sidemenu/qbxx');
-        }).error(function (error) {
-            $rootScope.hide();
-            $rootScope.notify("Invalid Username or password");
-        });
-    };
+  $scope.validateUser = function() {
+  //   var email = this.user.email;
+  //   var password = this.user.password;
+  //   if (!email || !password) {
+  //     $rootScope.notify("Please enter valid credentials");
+  //     return false;
+  //   }
+  //   $rootScope.show('Please wait.. Authenticating');
+  //   API.signin({
+  //     email: email,
+  //     password: password
+  //   }).success(function(data) {
+  //     $rootScope.setToken(email); // create a session kind of thing on the client side
+  //     $rootScope.hide();
+  //     $window.location.href = ('#/sidemenu/qbxx');
+  //   }).error(function(error) {
+  //     $rootScope.hide();
+  //     $rootScope.notify("Invalid Username or password");
+  //   });
+       $window.location.href = ('#/sidemenu/info');
+   };
 })
 
 .controller('SignUpCtrl', function($rootScope, $scope, API, $window) {
-  $scope.user = {
-        email: "",
-        password: "",
-        name: ""
-    };
+  // $scope.user = {
+  //   email: "",
+  //   password: "",
+  //   name: ""
+  // };
 
-    $scope.createUser = function () {
-      var email = this.user.email;
-        var password = this.user.password;
-        var uName = this.user.name;
-        if(!email || !password || !uName) {
-          $rootScope.notify("Please enter valid data");
-          return false;
-        }
-        $rootScope.show('Please wait.. Registering');
-        API.signup({
-            email: email,
-            password: password,
-            name: uName
-        }).success(function (data) {
-            $rootScope.setToken(email); // create a session kind of thing on the client side
-            $rootScope.hide();
-            $window.location.href = ('#/sidemenu/qbxx');
-        }).error(function (error) {
-            $rootScope.hide();
-          if(error.error && error.error.code == 11000)
-          {
-            $rootScope.notify("A user with this email already exists");
-          }
-          else
-          {
-            $rootScope.notify("Oops something went wrong, Please try again!");
-          }
-
-        });
-    };
+  $scope.createUser = function() {
+    // var email = this.user.email;
+    // var password = this.user.password;
+    // var uName = this.user.name;
+    // if (!email || !password || !uName) {
+    //   $rootScope.notify("Please enter valid data");
+    //   return false;
+    // }
+    // $rootScope.show('Please wait.. Registering');
+    // API.signup({
+    //   email: email,
+    //   password: password,
+    //   name: uName
+    // }).success(function(data) {
+    //   $rootScope.setToken(email); // create a session kind of thing on the client side
+    //   $rootScope.hide();
+    //   $window.location.href = ('#/sidemenu/qbxx');
+    // }).error(function(error) {
+    //   $rootScope.hide();
+    //   if (error.error && error.error.code == 11000) {
+    //     $rootScope.notify("A user with this email already exists");
+    //   } else {
+    //     $rootScope.notify("Oops something went wrong, Please try again!");
+    //   }
+    // });
+      $window.location.href = ('#/sidemenu/info');
+  };
 })
 
 /*
 Controller for the Home page
 */
 .controller('HomeCtrl', function($scope) {
-    // $scope.functions = [
-    //     { title: '情报信息' },
-    //     { title: '指挥控制' },
-    //     { title: '军事训练' },
-    //     { title: '教育管理' },
-    //     { title: '国防动员' },
-    //     { title: '通讯录' }
-    //   ];
+  // $scope.functions = [
+  //     { title: '情报信息' },
+  //     { title: '指挥控制' },
+  //     { title: '军事训练' },
+  //     { title: '教育管理' },
+  //     { title: '国防动员' },
+  //     { title: '通讯录' }
+  //   ];
 
-    // $scope.activeFunction = $scope.functions[Functions.getLastActiveIndex()];
+  // $scope.activeFunction = $scope.functions[Functions.getLastActiveIndex()];
 
-    //  // Called to select the given project
-    // $scope.selectFunction = function(project, index) {
-    //   $scope.activeFunction = project;
-    //   Functions.setLastActiveIndex(index);
+  //  // Called to select the given project
+  // $scope.selectFunction = function(project, index) {
+  //   $scope.activeFunction = project;
+  //   Functions.setLastActiveIndex(index);
 
-    //   switch(index){
-    //     case 0: $state.go('home.qbxx');break;
-    //     case 1: $state.go('home.zhkz');break;
-    //     case 2: $state.go('home.jsxl');break;
-    //   }
+  //   switch(index){
+  //     case 0: $state.go('home.qbxx');break;
+  //     case 1: $state.go('home.zhkz');break;
+  //     case 2: $state.go('home.jsxl');break;
+  //   }
 
-    //   $ionicSideMenuDelegate.toggleLeft(false);
-    // };
+  //   $ionicSideMenuDelegate.toggleLeft(false);
+  // };
 
 })
 
@@ -113,23 +111,165 @@ Controller for our 控制台
 Controller for our 情报信息
 */
 .controller('InfoCtrl', function($scope) {
-  $scope.comments = [
-  {
-    "name":"刘强",
-    "content":"高邮民社正常！",
-    "thumbnail": "img/ionic.png",
-  },
-  {
-    "name":"刘强",
-    "content":"高邮民社正常！",
-    "thumbnail": "img/ionic.png",
-  },
-  {
-  "name":"刘强",
-  "content":"高邮民社正常！",
-  "thumbnail": "img/ionic.png",
-  }
-  ];
+  $scope.user = {
+    username: "老鹰"
+  };
+  $scope.ups = [{
+    user: "老鹰",
+    content: "高邮民社出现异常!",
+    avatar:"https://avatars1.githubusercontent.com/u/2907617",
+    date: "2015-07-13",
+    likes: ["猎豹","狮子"],
+    comments: [{
+      user: "猎豹",
+      content: "没啥大事吧?",
+      date: "2015-07-14"
+    }, {
+      user: "狮子",
+      content: "啥异常说清楚啊？",
+      date: "2015-07-14 14:33",
+    }, {
+      user: "土狼",
+      content: "请配图",
+      date: "2015-07-14 14:36",
+    }]
+  }, {
+    user: "狮子",
+    content: "高邮有游行！",
+    avatar: "https://avatars3.githubusercontent.com/u/499550",
+    date: "2015-07-13",
+    likes: ["猎豹","狮子"],
+    comments: [{
+      user: "猎豹",
+      content: "没啥大事吧?",
+      date: "2015-7-14 14:30"
+    }, {
+      user: "狮子",
+      content: "啥异常说清楚啊？",
+      date: "2015-7-14 14:33",
+    }, {
+      user: "土狼",
+      content: "请配图",
+      date: "2015-7-14 14:36",
+    }]
+
+  }, {
+    user: "土狼",
+    content: "镇上出现暴走族！",
+    avatar: "https://avatars3.githubusercontent.com/u/376661",
+    date: "2015-7-13",
+    likes: ["猎豹","狮子"],
+    comments: [{
+      user: "猎豹",
+      content: "没啥大事吧?",
+      date: "2015-7-14 14:30"
+    }, {
+      user: "狮子",
+      content: "啥异常说清楚啊？",
+      date: "2015-7-14 14:33",
+    }, {
+      user: "土狼",
+      content: "请配图",
+      date: "2015-7-14 14:36",
+    }]
+  }];
+
+  $scope.posts = [{
+    user: "老鹰",
+    content: "高邮民社一切正常!",
+    avatar:"https://avatars1.githubusercontent.com/u/2907617",
+    date: "2015-07-13",
+    likes: ["猎豹","狮子"],
+    comments: [{
+      user: "猎豹",
+      content: "没啥大事吧?",
+      date: "2015-07-14"
+    }, {
+      user: "狮子",
+      content: "啥异常说清楚啊？",
+      date: "2015-07-14 14:33",
+    }, {
+      user: "土狼",
+      content: "请配图",
+      date: "2015-07-14 14:36",
+    }]
+  }, {
+    user: "狮子",
+    content: "泰州一切安好！",
+    avatar: "https://avatars3.githubusercontent.com/u/499550",
+    date: "2015-07-13",
+    likes: ["猎豹","狮子"],
+    comments: [{
+      user: "猎豹",
+      content: "没啥大事吧?",
+      date: "2015-7-14 14:30"
+    }, {
+      user: "狮子",
+      content: "啥异常说清楚啊？",
+      date: "2015-7-14 14:33",
+    }, {
+      user: "土狼",
+      content: "请配图",
+      date: "2015-7-14 14:36",
+    }]
+
+  }, {
+    user: "土狼",
+    content: "镇上一切安好，请祖国人命放心",
+    avatar: "https://avatars3.githubusercontent.com/u/376661",
+    date: "2015-7-13",
+    likes: ["猎豹","狮子"],
+    comments: [{
+      user: "猎豹",
+      content: "没啥大事吧?",
+      date: "2015-7-14 14:30"
+    }, {
+      user: "狮子",
+      content: "啥异常说清楚啊？",
+      date: "2015-7-14 14:33",
+    }, {
+      user: "土狼",
+      content: "请配图",
+      date: "2015-7-14 14:36",
+    }]
+  }];
+
+
+  $scope.upLike =function (index) {
+     var indexUser = $scope.ups[index].likes.indexOf($scope.user.username);
+     if (indexUser > -1) {
+        $scope.ups[index].likes.splice(indexUser, 1);
+     }else{
+        $scope.ups[index].likes.push($scope.user.username);
+      }
+  };
+
+  $scope.isUpLike =  function (index) {
+      var indexUser = $scope.ups[index].likes.indexOf($scope.user.username);
+      if(indexUser != -1){
+        return true;
+      }else{
+        return false;
+      }
+  };
+
+  $scope.postsLike =function (index) {
+     var indexUser = $scope.posts[index].likes.indexOf($scope.user.username);
+     if (indexUser > -1) {
+        $scope.posts[index].likes.splice(indexUser, 1);
+     }else{
+        $scope.posts[index].likes.push($scope.user.username);
+      }
+  };
+
+  $scope.isPostsLike =  function (index) {
+      var indexUser = $scope.posts[index].likes.indexOf($scope.user.username);
+      if(indexUser != -1){
+        return true;
+      }else{
+        return false;
+      }
+  };
 
 })
 
@@ -159,30 +299,6 @@ Controller for our 教育管理
 Controller for our 国防动员
 */
 .controller('GfdyCtrl', function($scope) {
-
-})
-
-/*
-Controller for our 紧急上报
-*/
-.controller('TabCtrl', function($scope) {
-  $scope.comments = [
-  {
-    "name":"刘强",
-    "content":"高邮民社正常！",
-    "thumbnail": "img/ionic.png",
-  },
-  {
-    "name":"刘强",
-    "content":"高邮民社正常！",
-    "thumbnail": "img/ionic.png",
-  },
-  {
-  "name":"刘强",
-  "content":"高邮民社正常！",
-  "thumbnail": "img/ionic.png",
-  }
-  ];
 
 })
 
