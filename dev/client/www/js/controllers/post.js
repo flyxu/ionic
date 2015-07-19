bcd72ad4a6093f0fe6ed72d7a8c48b4cee47a07f
rdmclin2 <rdmclin2@163.com>
2015-07-19 19:01:53 +0800

    beatutify create info list

--- dev/client/www/js/controllers/post.js
+++ dev/client/www/js/controllers/post.js
@@ -2,10 +2,31 @@ angular.module('fcws.controllers')
 .controller('PostCtrl', function($scope,post,$ionicPopup){
     $scope.post = post;
 
+    $scope.user = {
+      username: "老鹰"
+    };
+
+    $scope.Like =function () {
+       var indexUser = $scope.post.likes.indexOf($scope.user.username);
+       if (indexUser > -1) {
+          $scope.post.likes.splice(indexUser, 1);
+       }else{
+          $scope.post.likes.push($scope.user.username);
+        }
+    };
+
+    $scope.isLike =  function () {
+        var indexUser = $scope.post.likes.indexOf($scope.user.username);
+        if(indexUser != -1){
+          return true;
+        }else{
+          return false;
+        }
+    };
+
     // Triggered on a button click, or some other target
 $scope.showPopup = function() {
   $scope.data = {};
-
   // An elaborate, custom popup
   var myPopup = $ionicPopup.show({
     template: '<input type="text" ng-model="data.comment">',
@@ -29,12 +50,14 @@ $scope.showPopup = function() {
   });
   myPopup.then(function(res) {
     console.log('Tapped!', res);
-    $scope.post.comments.push({
-      user: "老鹰",
-      content: res,
-      date: new Date().toLocaleDateString().toString()
-    }
-  );
+    if(res){
+      $scope.post.comments.push({
+        user: "老鹰",
+        content: res,
+        date: new Date().toLocaleDateString().toString()
+      }
+    );
+  }
   });
   // $timeout(function() {
   //    myPopup.close(); //close the popup after 3 seconds for some reason
